7ff94f2879d9e3bfa679e3d6a8e0e01d128e970d
sucharita4867 <sucharitasardar7@gmail.com>
2026-03-12 23:35:20 +0530

    complete logo components

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import logo from "/Logo.png";
 
 const Logo = () => {
-  return <div>Logo</div>;
+  return (
+    <div className="flex justify-center items-center gap-2">
+      <img src={logo} alt="" />
+      <p className="text-[#212337] text-2xl font-bold">Fresh Harvests</p>
+    </div>
+  );
 };
 
 export default Logo;
