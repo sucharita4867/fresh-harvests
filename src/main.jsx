42eae96b448d4b804649a8d1d6e6fda01b26828c
sucharita4867 <sucharitasardar7@gmail.com>
2026-03-12 17:13:29 +0530

    fixed tailwiend css problem and show daisyui navbar

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import { RouterProvider } from "react-router";
 import router from "./router/router";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
