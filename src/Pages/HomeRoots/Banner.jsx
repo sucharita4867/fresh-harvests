ebe0da7e4f99485c84c0fd6b9a50f51e5b813936
sucharita4867 <sucharitasardar7@gmail.com>
2026-03-15 23:34:10 +0530

    start positions

--- src/Pages/HomeRoots/Banner.jsx
+++ src/Pages/HomeRoots/Banner.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import bg from "./../../assets/Bg.png";
+import bg2 from "./../../assets/Bg (1).png";
 
 const Banner = () => {
   return (
-    <div>
+    <div className="relative">
       <img src={bg} alt="" />
+      <img src={bg2} className="absolute" alt="" />
     </div>
   );
 };
