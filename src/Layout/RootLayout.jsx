17308730fd176c939e188fc27cc3b8b61ae71787
sucharita4867 <sucharitasardar7@gmail.com>
2026-03-21 22:52:12 +0530

    update code

--- src/Layout/RootLayout.jsx
+++ src/Layout/RootLayout.jsx
@@ -5,9 +5,14 @@ import { Outlet } from "react-router";
 
 const RootLayout = () => {
   return (
-    <div>
-      <Navbar />
-      <Outlet />
+    <div className="relative">
+      {/* Navbar কে absolute করে z-index বাড়িয়ে দেওয়া হলো যাতে এটি ইমেজের উপরে থাকে */}
+      <div className="absolute top-0 left-0 w-full z-50">
+        <Navbar />
+      </div>
+      <main>
+        <Outlet />
+      </main>
       <Footer />
     </div>
   );
