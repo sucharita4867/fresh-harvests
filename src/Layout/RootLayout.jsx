79684583ebfe7a680f59732558286b80644ee3c7
sucharita4867 <sucharitasardar7@gmail.com>
2026-03-21 23:42:42 +0530

    update front

--- src/Layout/RootLayout.jsx
+++ src/Layout/RootLayout.jsx
@@ -6,7 +6,6 @@ import { Outlet } from "react-router";
 const RootLayout = () => {
   return (
     <div className="relative">
-      {/* Navbar কে absolute করে z-index বাড়িয়ে দেওয়া হলো যাতে এটি ইমেজের উপরে থাকে */}
       <div className="absolute top-0 left-0 w-full z-50">
         <Navbar />
       </div>
--- src/Pages/HomeRoots/Banner.jsx
+++ src/Pages/HomeRoots/Banner.jsx
@@ -5,26 +5,22 @@ import bg2 from "./../../assets/Bg (1).png";
 const Banner = () => {
   return (
     <div className="relative w-full h-[600px] overflow-hidden">
-      {/* মেইন সাদা ব্যাকগ্রাউন্ড ইমেজ */}
       <img
         src={bg}
         alt="background"
         className="absolute top-0 left-0 w-full h-full object-cover -z-10"
       />
 
-      {/* ডানদিকের সবুজ ইমেজটি যা একদম টপ-রাইট এ ফিক্সড থাকবে */}
       <img
         src={bg2}
         alt="green background"
         className="absolute top-0 right-0 h-full w-auto object-cover -z-10"
       />
 
-      {/* ব্যানারের ভেতরের কন্টেন্ট (Text, Buttons) এখানে লিখুন */}
       <div className="relative z-10 pt-40 px-10">
-        <h1 className="text-5xl font-bold">
+        <h1 className="text-5xl ">
           Fresh Fruits and <br /> Vegetables
         </h1>
-        {/* বাকি কন্টেন্ট... */}
       </div>
     </div>
   );
