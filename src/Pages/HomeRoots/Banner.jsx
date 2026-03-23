737b9c7d728835db127e64feb734dc16eb59cc9e
sucharita4867 <sucharitasardar7@gmail.com>
2026-03-23 22:31:15 +0530

    added banner img and styles

--- src/Pages/HomeRoots/Banner.jsx
+++ src/Pages/HomeRoots/Banner.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import bg from "./../../assets/Bg.png";
 import bg2 from "./../../assets/Bg (1).png";
+import img from "./../../assets/bannerImg.png";
 
 const Banner = () => {
   return (
@@ -15,11 +16,16 @@ const Banner = () => {
         alt="green background"
         className="absolute top-0 right-0 h-full w-auto object-cover -z-10"
       />
-      <div className="relative z-10 pt-20 px-10 border border-red-600">
-        <h1 className="text-[#749B3F] text-xl font-medium">
-          Welcome to Fresh Harvest
-        </h1>
-        <h3 className="text-5xl font-bold">Fresh Fruits and Vegetables</h3>
+      <div className="relative z-10 pt-20 px-10 flex  items-center border border-red-600">
+        <div>
+          <h1 className="text-[#749B3F] text-xl font-medium">
+            Welcome to Fresh Harvest
+          </h1>
+          <h3 className="text-5xl font-bold">Fresh Fruits and Vegetables</h3>
+        </div>
+        <figure>
+          <img className="w-[50%] h-[50%]" src={img} alt="" />
+        </figure>
       </div>
     </div>
   );
